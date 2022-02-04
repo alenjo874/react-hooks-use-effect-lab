f2ceaf19fa9fd0848d1255843f27548c7574c3f0
Alen Jo <alenjo874@gmail.com>
2022-02-04 15:50:41 -0500

    commit tree 36863470f6f6bb9183c2e72374ac375c5d217ecb

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1);
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(timeout);
+    };
+  });
+
+  if (timeRemaining === 0) {
+    onAnswered(false);
+    return setTimeRemaining(10);
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
